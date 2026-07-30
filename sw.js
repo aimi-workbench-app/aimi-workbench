26139f06cd025db3f6bca85b743bc37403fbdbe8
aimi-workbench-app <1403166294@qq.com>
2026-07-30 23:16:11 +0800

    clear old cache sw v3

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-// 艾米工作台 Service Worker - 离线缓存
-const CACHE = 'aimi-v2';
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll([
-        './index.html',
-        './tailwind.js',
-        './manifest.json',
-        './apple-touch-icon.png',
-        './icon-192.png',
-        './icon-512.png',
-        './data/trends.json'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request);
-    })
-  );
-});
+// dino sw v3 - clear all old cache
+const CACHE = 'dino-v3';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { if (e.request.mode === 'navigate') { e.respondWith(fetch(e.request).then(res => { const c = res.clone(); caches.open(CACHE).then(cache => cache.put(e.request, c)); return res; }).catch(() => caches.match(e.request))); } });
